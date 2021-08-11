4c79c653bad305d9aef56ee6b20a848e573998ca
LucidaLu <luyiren12@gmail.com>
2021-08-11 09:41:18 +0800

    m

--- parser/build.sh
+++
@@ -1,4 +0,0 @@
-g++ recursive-descent.cpp -o recursive-descent -Wall -Wextra -std=c++17 -fsanitize=undefined -g
-g++ ll1.cpp -o ll1 -Wall -Wextra -std=c++17 -fsanitize=undefined -g
-g++ lr1.cpp -o lr1 -Wall -Wextra -std=c++17 -fsanitize=undefined -g
-flex lex.l && bison -vtdy yacc.y && gcc -o flex-bison y.tab.c lex.yy.c && gcc -o cc y.tab.c lex.yy.c
